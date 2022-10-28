43f2cf631ce4c8b963c70da2ed0f2d38cc15cad8
Eduard Canal <eduard.canal.m@gmail.com>
2022-10-28 10:59:54 +0200

    fixed AGAIN same bug as last commit

--- routes/toy.routes.js
+++ routes/toy.routes.js
@@ -38,7 +38,7 @@ router.get("/:idtoy/detail", async (req, res, next) => {
     let reserved = false;
     let reservedButton = true
     if (req.session.activeUser !==undefined) {
-      if (actualUserReserve.toyReserved !== null && actualUserReserve.toyReserved !== undefined){
+      if (actualUserReserve !== undefined && actualUserReserve.toyReserved !== null && actualUserReserve.toyReserved !== undefined){
         actualUserReserve = await User.findById(req.session.activeUser._id).populate("toyReserved")  //.select("toyReserved")
         const { _id }= actualUserReserve.toyReserved
         const definitiveID = JSON.stringify(_id).split(`"`)
